c1e85f953727a4dfafa2c14fb79add3457f03199
arman <git.user.armanduskal@gmail.com>
2025-08-26 00:44:40 +0300

    доработка 2

---
+++ src/main/java/Box.java
@@ -0,0 +1,18 @@
+public class Box {
+    public int getSize() {
+        return size;
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    private int size;
+
+    }
+
+
