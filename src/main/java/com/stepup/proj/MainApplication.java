6e4503df5be2496a8bcae38565365ad2172da157
arman <git.user.armanduskal@gmail.com>
2025-08-26 00:16:38 +0300

    доработка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("111111");
+        System.out.println("saas");
     }
 }
