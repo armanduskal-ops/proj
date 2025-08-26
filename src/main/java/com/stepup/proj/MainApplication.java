bedeae1ca6cbafc1970cab27b1351bb3216a446f
arman <git.user.armanduskal@gmail.com>
2025-08-26 20:22:15 +0300

    улучшили логику

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,7 +3,8 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("111111");
+        System.out.println("Bye World");
+        System.out.println("222");
         System.out.println("saas");
     }
 }
